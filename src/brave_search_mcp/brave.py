f9362fab2bf7a3f14556375e06270d1b0537ace6
a.pridachin <a.s.pridachin@gmail.com>
2024-12-12 10:51:20 +0100

    add params to web search

--- src/brave_search_mcp/brave.py
+++ src/brave_search_mcp/brave.py
@@ -1,19 +1,36 @@
 import json
+import re
 from urllib.parse import urljoin
 
 import httpx
 from mcp import McpError, types as types
 
 from utils.config import Config
+from brave_search_mcp.codes import CountryCode, LanguageCode
 
 config = Config()
 
-async def web_search(query: str) -> str:
+count_pattern = r"^\d+$"
+freshness_pattern = r"^\d{4}-\d{2}-\d{2}to\d{4}-\d{2}-\d{2}$"
+
+
+async def web_search(
+    query: str,
+    count: str = '10', # integer is not supported in MCP Inspector v0.3.0
+    country: CountryCode = CountryCode.ALL_REGIONS.value,
+    search_lang: LanguageCode = LanguageCode.ENGLISH.value,
+    freshness: str | None = None,
+) -> str:
+    params = {
+        "q": query,
+        "count": _validate_count(count),
+        "country": country,
+        "search_lang": search_lang,
+        "freshness": _validate_freshness(freshness) if freshness else None,
+    }
     response = await _call_brave(
         url="/res/v1/web/search",
-        params={
-            "q": query,
-        }
+        params=params,
     )
     data = response.json()
     results = _parse_search_results(data)
@@ -40,6 +57,18 @@ async def _call_brave(url: str, params: dict | None = None) -> httpx.Response:
         raise McpError(types.INTERNAL_ERROR, f"Brave API error: {str(e)}")
 
 
+def _validate_count(count: str) -> str | None:
+    if re.match(count_pattern, count):
+        return count
+    return None
+
+
+def _validate_freshness(freshness: str) -> str | None:
+    if re.match(freshness_pattern, freshness):
+        return freshness
+    return None
+
+
 def _parse_search_results(data: dict) -> dict:
     search_results = data["web"]["results"]
     return {
@@ -48,6 +77,8 @@ def _parse_search_results(data: dict) -> dict:
                 "title": r["title"],
                 "url": r["url"],
                 "description": r["description"],
-            } for r in search_results
+                "language": r["language"],
+            }
+            for r in search_results
         ]
     }
--- src/brave_search_mcp/server.py
+++ src/brave_search_mcp/server.py
@@ -1,12 +1,14 @@
 import asyncio
 import logging
 
+from jsonschema import validate, ValidationError
 import mcp.server.stdio
 import mcp.types as types
 from mcp.server import NotificationOptions, Server
 from mcp.server.models import InitializationOptions
 
-from brave_search_mcp.brave import web_search
+from brave_search_mcp.brave import web_search, freshness_pattern, count_pattern
+from brave_search_mcp.codes import LanguageCode, CountryCode
 from utils.config import Config
 
 config = Config()
@@ -21,13 +23,7 @@ async def handle_list_tools() -> list[types.Tool]:
         types.Tool(
             name="web_search",
             description="Search web",
-            inputSchema={
-                "type": "object",
-                "properties": {
-                    "query": {"type": "string"},
-                },
-                "required": ["query"],
-            },
+            inputSchema=inputSchema,
         )
     ]
 
@@ -46,13 +42,9 @@ async def handle_call_tool(
     if not arguments:
         raise ValueError("Missing arguments")
 
-    query = arguments.get("query")
-
-    if not query:
-        raise ValueError("Missing query")
-
+    validate_arguments(arguments)
     tool_function = tools[name]
-    result = await tool_function(query)
+    result = await tool_function(**arguments)
 
     return [
         types.TextContent(
@@ -60,6 +52,26 @@ async def handle_call_tool(
             text=result,
         )
     ]
+
+
+inputSchema = {
+    "type": "object",
+    "properties": {
+        "query": {"type": "string"},
+        "count": {"type": "string", "pattern": count_pattern},
+        "country": {"type": "string", "enum": [e for e in CountryCode]},
+        "search_lang": {"type": "string", "enum":  [e for e in LanguageCode]},
+        "freshness": {"type": "string", "pattern": freshness_pattern},
+    },
+    "required": ["query"],
+}
+
+
+def validate_arguments(arguments: dict):
+    try:
+        validate(instance=arguments, schema=inputSchema)
+    except ValidationError as e:
+        raise ValueError(f"Invalid arguments: {e.message}") from e
 
 
 async def main():
